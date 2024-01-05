d2aa31f3ec2d89a1454c98595113251375fff1b7
mehdi39 <jahid4k@gmail.com>
2024-01-05 22:37:24 +0600

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_first_app/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Icon(Icons.menu, color: tdBlack, size: 30),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpg'),
+            ),
+          )
+        ],),
+      ),
+      body: Container(
+        child: Text('This is Home Screen'),
+      ),
+    );
+  }
+}
