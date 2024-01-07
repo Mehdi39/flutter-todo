c6a0b0ae56e36519e73098df9386bfb3db96af3a
mehdi39 <jahid4k@gmail.com>
2024-01-07 22:17:32 +0600

    feat: todo

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,17 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_first_app/constants/colors.dart';
+import '../constants/colors.dart';
+import '../widgets/todo_item.dart';
+import '../model/todo.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+
+  final todoList = ToDo.todoList();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 15
+        ),
+        child: Column(
           children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 50, bottom: 20),
+                    child: Text(
+                      'All ToDos',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  for ( ToDo todo in todoList)
+                  ToDoItem(todo: todo),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGray),
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
           Icon(Icons.menu, color: tdBlack, size: 30),
           Container(
             height: 40,
@@ -21,10 +88,7 @@ class Home extends StatelessWidget {
               child: Image.asset('assets/images/avatar.jpg'),
             ),
           )
-        ],),
-      ),
-      body: Container(
-        child: Text('This is Home Screen'),
+        ],
       ),
     );
   }
